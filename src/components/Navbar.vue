aacd17daa7edf7d4f2cff7ee828ee49bd3fc7fe9
Luiz Phelippe <aguiarluizphelippe@gmail.com>
2026-03-09 02:51:46 -0300

    style: ajuste na navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,73 +1,41 @@
 <template>
+  <!-- Adicionar alguma logo ou nome do site aqui -->
   <nav
-    class="limelight-regular bg-black h-[3rem] flex items-center justify-between px-4 w-[100%] sticky top-0 z-50"
-  >
-    <button
-      v-show="pathAtual === '/documentacao'"
-      @click="$emit('toggle-menu')"
-      class=" rounded min-[800px]:hidden w-10 h-10 flex items-center justify-center border-2 border-black bg-yellow-300 hover:bg-yellow-400 hover:shadow-[2px_2px_0_0] transition-all font-bold"
-    >
-      <svg
-        v-if="!state.mudarBotao"
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        fill="currentColor"
-        class="bi bi-list"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-        />
+    class="limelight-regular bg-yellow-300 h-[3rem] flex items-center justify-end px-4 w-[100%] sticky top-0 z-50 border-b-3 border-black">
+    <button v-show="pathAtual === '/documentacao'" @click="$emit('toggle-menu')"
+      class="mr-auto shadow-[2px_2px_0_0_#FFDF20] hover:border-2 hover:border-black hover:shadow-[2px_2px_0_0_#000] border-2 border-transparent cursor-pointer">
+      <svg v-if="!state.mudarBotao" xmlns="http://www.w3.org/2000/svg" width="28" height="auto" fill="currentColor"
+        class="bi bi-list" viewBox="0 0 16 16">
+        <path fill-rule="evenodd"
+          d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
       </svg>
-      <svg
-        v-else
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        fill="currentColor"
-        class="bi bi-x"
-        viewBox="0 0 16 16"
-      >
+      <svg v-else xmlns="http://www.w3.org/2000/svg" width="28" height="auto" fill="currentColor" class="bi bi-x"
+        viewBox="0 0 16 16">
         <path
-          d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"
-        />
+          d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
       </svg>
     </button>
     <ul class="flex justify-end gap-6 me-[15px]">
       <li>
-        <router-link to="/" class="text-yellow-300 font-bold hover:text-white transition-colors"
-          >Home</router-link
-        >
+        <router-link to="/"
+          class="nav-link border-2 border-transparent shadow-[2px_2px_0_0_#FFDF20]">Início</router-link>
       </li>
       <li>
-        <router-link
-          to="/documentacao"
-          class="text-yellow-300 font-bold hover:text-white transition-colors"
-          >Documentação</router-link
-        >
-      </li>
-        <li>
-          <router-link
-            to="/sobre-nos"
-            class="text-yellow-300 font-bold hover:text-white transition-colors"
-            >Sobre Nós</router-link
-          >
-        </li>
-      <li>
-        <router-link
-          to="/pratica/1"
-          class="text-yellow-300 font-bold hover:text-white transition-colors"
-          >Prática</router-link
-        >
+        <router-link to="/documentacao"
+          class="nav-link border-2 border-transparent shadow-[2px_2px_0_0_#FFDF20]">Documentação</router-link>
       </li>
       <li>
-        <router-link
-          to="/ferramentas"
-          class="text-yellow-300 font-bold hover:text-white transition-colors"
-          >Ferramentas</router-link
-        >
+        <router-link to="/sobre-nos"
+          class="nav-link border-2 border-transparent shadow-[2px_2px_0_0_#FFDF20]">Sobre
+          Nós</router-link>
+      </li>
+      <li>
+        <router-link to="/pratica/1"
+          class="nav-link border-2 border-transparent shadow-[2px_2px_0_0_#FFDF20]">Prática</router-link>
+      </li>
+      <li>
+        <router-link to="/ferramentas"
+          class="nav-link border-2 border-transparent shadow-[2px_2px_0_0_#FFDF20]">Ferramentas</router-link>
       </li>
     </ul>
   </nav>
@@ -112,6 +80,21 @@ a {
   padding: 4px 8px;
   border-radius: 2px;
   transition: all 0.2s ease;
+}
+
+a.nav-link.router-link-exact-active {
+  border-color: black;
+  box-shadow: 2px 2px 0 0 #000;
+}
+
+a.nav-link.router-link-exact-active:hover {
+  background-color: rgb(250 250 250);
+}
+
+a.nav-link:not(.router-link-exact-active):hover {
+  border-color: black;
+  box-shadow: 2px 2px 0 0 #000;
+  background-color: rgb(250 250 250);
 }
 
 li {
